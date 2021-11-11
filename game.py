7266749bed1c919025626e5bcc3b945b592135ea
Lewis <lewisdwall@gmail.com>
2021-11-11 20:22:16 +0000

    A working game

--- game.py
+++ game.py
@@ -1,9 +1,7 @@
 from player import Player
+import random
 
-name = input("Please enter your name: ")
-
-def play_game():
-    pass
+input_name = input("Please enter your name: ")
 
 def player_choice():
     choice = input("Please choose Rock, Paper or Scissors: ")
@@ -14,11 +12,33 @@ def create_player(name, choice):
     return new_player
 
 def create_computer_player():
-    pass
+    moves = ["Rock", "Paper", "Scissors"]
+    computer_choice = random.choice(moves)
+    computer = Player("Computer", computer_choice)
+    return computer
 
 def get_winner(player, computer):
-    pass
+    win_lookup = {
+        "scissors" : "paper",
+        "rock" : "scissors",
+        "paper" : "rock"
+    }
 
-player = create_player("Ally", "Rock")
-print("Name " + player.name)
-print("Choice " + player.choice) 
+    if win_lookup[player.choice.lower()] == computer.choice.lower():
+        winner = player
+    elif win_lookup[computer.choice.lower()] == player.choice.lower():
+        winner = computer 
+    else: 
+        winner = None 
+    
+    return winner 
+
+player_choice = player_choice()
+player = create_player(input_name, player_choice)
+computer = create_computer_player()
+winner = get_winner(player, computer)
+
+if winner == None: 
+    print("It's a draw")
+else: 
+    print(f"{winner.name} won by playing {winner.choice}")
